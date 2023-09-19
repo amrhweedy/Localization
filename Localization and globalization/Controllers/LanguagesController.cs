2f6c18ef50a4f9922102c755c54756515b2dab5d
unknown <amrhweedy@gamil.com>
2023-09-19 06:46:24 -0700

    first

---
+++ Localization and globalization/Controllers/LanguagesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace Localizationandglobalization.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LanguagesController : ControllerBase
+    {
+        private readonly IStringLocalizer<LanguagesController> localizer;  // i use it to read the values from the files in the resources folder
+
+        public LanguagesController(IStringLocalizer<LanguagesController> localizer)
+        {
+            this.localizer = localizer;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = $" {localizer["Welcome"].Value} , {localizer["Name"].Value}";  
+
+            return Ok(result);   // "Welocme" is the key in the file in the Resources folder
+                                               // when i run the app without creating the resources folder the result is 
+        }
+    }
+}
---
+++ Localization and globalization/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+
+namespace Localizationandglobalization
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            //بحدد بيها فين مكان الريسورسيس فايل اللي هيكون فيه المرادفات بتاعت الكلمات اللي بستخدمها مثلا كلمه ويلكام بالانجليزي هتكون بتساوي مرحبا بالعربي
+            //فلازم اعرف الابكيشن لما اليوزر يستخدم الانجليزي يبقي يستخدم ويلكام لو مستخدم عربي يبقي يستخدم مرحبا 
+            // فلازم اعرفه مكان الفولدر اللي هتكون فيه الفايلات اللي هو بيقري منها القيم دي
+            builder.Services.AddLocalization(opt => opt.ResourcesPath = "Resources"); //  Resources >> the name of folder
+
+
+            builder.Services.Configure<RequestLocalizationOptions>(opt =>
+            {
+                var supportedCultures = new[]
+                {
+                     new CultureInfo("en"),
+                     new CultureInfo("ar"),
+                     new CultureInfo("de"),
+                     new CultureInfo("fr")
+                };
+
+                opt.DefaultRequestCulture = new RequestCulture(culture: "en", uiCulture: "en");
+                opt.SupportedCultures = supportedCultures;
+                opt.SupportedUICultures = supportedCultures;
+            }
+            );
+
+           
+
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger(); 
+                app.UseSwaggerUI();
+            }
+            //step 3
+            app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
